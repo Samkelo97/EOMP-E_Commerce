d3466faf7cb0b53771059143dbc2273ac6424889
Samkelo <sammpokela@gmail.com>
2023-06-14 17:00:41 +0200

    commit6

---
+++ JS/Products.js
@@ -0,0 +1,401 @@
+let Items = [
+  
+{
+  id:13,
+  image:"https://i.postimg.cc/7PcT7hPt/Nike-Men-s-Ecru-Sweat-Pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 356",
+brand:"Nike"
+},
+{
+  id:14,
+  image:"https://i.postimg.cc/zXKSGKwF/Nike-Men-s-Black-NSW-Club-Jogger-Pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 259",
+brand:"Nike"
+},
+{
+  id:15,
+  image:"https://i.postimg.cc/yx99WCR8/Nike-Club-Blue-Cargo-Pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 356",
+brand:"Nike"
+}
+,
+{
+  id:16,
+  image:"https://i.postimg.cc/9fkHF6VS/Mens-adidas-Tiro-23-Club-Training-Black-Pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 099",
+brand:"addidas"
+}
+,
+{
+  id:17,
+  image:"https://i.postimg.cc/d0kSnLPH/adidas-Originals-Men-s-Black-Pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"2 299",
+brand:"Addidas"
+}
+,
+{
+  id:18,
+  image:"https://i.postimg.cc/tC4k2YF3/adidas-Originals-Men-s-3-stripes-Olive-Green-Cargo-Pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 899",
+brand:"Addidas"
+}
+,
+{
+  id:19,
+  image:"https://i.postimg.cc/9QGVmmQn/under-armor-pique-track-pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 999",
+brand:"UA"
+}
+,
+{
+  id:20,
+  image:"https://i.postimg.cc/k4DW3hvf/under-armor-pique-blue-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 599",
+brand:"UA"
+}
+,
+{
+  id:21,
+  image:"https://i.postimg.cc/Kz6XQcfL/under-armor-grey-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 399",
+brand:"UA"
+}
+,
+{
+  id:22,
+  image:"https://i.postimg.cc/N0CgccsC/new-balance-AT-pants-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 399",
+brand:"NB"
+}
+,
+{
+  id:23,
+  image:"https://i.postimg.cc/SQCpZ8Mv/impact-Core-NB-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 299",
+  brand:"NB"
+
+}
+,
+{
+  id:24,
+  image:"https://i.postimg.cc/jdPXDH1t/grey-NB-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"1 899",
+  brand: "NB",
+}
+,
+
+{
+  id: 1,
+  image:"https://i.postimg.cc/bvLC0bxM/Nike-Men-s-Olive-Green-Windbreaker-Jacket-removebg-preview.png",
+  desc: "Rolex champagne Daytona",
+  price: "1 800",
+},
+{
+  id: 2,
+  image:"https://i.postimg.cc/9QbKbvVJ/Nike-Men-s-Green-Harrington-Jacket-removebg-preview.png",
+  desc: "Rolex submariner in oystersteel",
+  price: "2 899",
+},
+{
+  id: 3,
+  image:"https://i.postimg.cc/hjT0ydTc/Nike-Sportswear-Storm-FIT-Windrunner-Men-s-PRIMALOFT-Black-Filled-Parka-removebg-preview.png c4",
+  desc: "Sahmed & sons",
+  price: "1 899",
+},
+{
+  id: 4,
+  image:"https://i.postimg.cc/2ypH0swt/Mens-adidas-Marathon-Warm-up-Blue-Jacket-removebg-preview.png",
+  desc: "Police Menso Watch clean silver fang for men",
+  price: "1 899",
+},
+{
+  id: 5,
+  image:"https://i.postimg.cc/ncFKnVCk/Men-s-adidas-Designed-for-Gameday-Black-Full-Zip-Hoodie-removebg-preview-1.png",
+  desc: "Police Malawi Multifunction Leather strap",
+  price: "1 700",
+},
+{
+    id:6,
+    image:"https://i.postimg.cc/qRZrgvp3/adidas-Originals-Men-s-Multicolour-Windbreaker-removebg-preview.png",
+    desc:"Police Raho Watch skull addition for men",
+    price:"1 499",
+
+},
+{
+  id:7,
+  image:"https://i.postimg.cc/T1xTzYJM/winter-AU-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"2 399",
+
+},
+{
+id:8,
+image:"https://i.postimg.cc/HnNBJjZF/gold-gear-AU-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"4 356",
+
+},
+{
+id:9,
+image:"https://i.postimg.cc/3N2Fw9G0/fore-front-AU-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"4 356",
+
+},
+{
+id:10,
+image:"https://i.postimg.cc/ZYgqY4YN/Mens-New-Balance-Accelerate-Half-Zip-Black-Top-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"4 356",
+
+},
+{
+id:11,
+image:"https://i.postimg.cc/pdhH6n81/New-Balance-Men-s-Hoops-Black-Hoodie-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"4 356",
+
+},
+{
+id:3,
+image:"https://i.postimg.cc/7Yfm7Myc/New-Balance-All-Terrain-Rust-Jacket-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"4 356",
+
+},
+{
+  id: 1,
+  image:"https://i.postimg.cc/nh2VM21q/Nike-Neutral-Olive-Metal-Swoosh-Cap-removebg-preview.png",
+  desc: "Rolex champagne Daytona",
+  price: "399",
+},
+{
+  id: 2,
+  image:"https://i.postimg.cc/xdF6jrJh/Nike-Futura-Oxen-Brown-Cap-removebg-preview.png",
+  desc: "Rolex submariner in oystersteel",
+  price: "399",
+},
+{
+  id: 3,
+  image:"https://i.postimg.cc/Z5QNCJH8/Nike-Sportswear-Heritage86-Black-Cap-removebg-preview.png",
+  desc: "Sahmed & sons",
+  price: "399",
+},
+{
+  id: 4,
+  image:"https://i.postimg.cc/L6jvg9sF/adidas-Originals-MC-Multicolour-Cap-removebg-preview.png",
+  desc: "Police Menso Watch clean silver fang for men",
+  price: "359",
+},
+{
+  id: 5,
+  image:"https://i.postimg.cc/s2xgGLK5/adidas-Originals-Black-Satin-Cap-removebg-preview.png",
+  desc: "Police Malawi Multifunction Leather strap",
+  price: "359",
+},
+{
+    id:6,
+    image:"https://i.postimg.cc/ZYr6V9F0/adidas-Essential-White-Running-Cap-removebg-preview.png",
+    desc:"Police Raho Watch skull addition for men",
+    price:"399",
+
+},
+{
+  id:7,
+  image:"https://i.postimg.cc/PNnvYv4G/Under-Armour-Isochill-Black-Run-Cap-removebg-preview-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"399",
+
+},
+{
+id:8,
+image:"https://i.postimg.cc/Nj3fd22L/Under-Armour-Blitzing-3-0-Royal-Blue-Cap-removebg-preview-1.png",
+desc:"Police Raho Watch skull addition for men",
+price:"399",
+
+},
+{
+id:9,
+image:"https://i.postimg.cc/85hH7vMv/Under-Armour-White-Blitzing-Cap-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"349",
+
+},
+{
+id:10,
+image:"https://i.postimg.cc/63YD8Fdn/New-Balance-Tabacco-Classics-Brown-Cap-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"489",
+
+},
+{
+id:11,
+image:"https://i.postimg.cc/vmyj3Gh5/New-Balance-Black-Run-Cap-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"399",
+
+},
+{
+id:12,
+image:"https://i.postimg.cc/VLbVwJHj/New-Balance-Blue-Haze-Running-Visor-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"399",
+},
+{
+  id: 1,
+  image:"https://i.postimg.cc/V6LVC5ng/Nike-Men-s-Air-Max-Black-Sneaker-removebg-preview.png",
+  desc: "Rolex champagne Daytona",
+  price: "2 799",
+},
+{
+  id: 2,
+  image:"https://i.postimg.cc/XYjHhpDb/Nike-Junior-Freak-4-Black-Sneaker-Nike-Junior-Freak-4-Black-Sneaker-removebg-preview.png",
+  desc: "Rolex submariner in oystersteel",
+  price: "2 399",
+},
+{
+  id: 3,
+  image:"https://i.postimg.cc/5tH3dvBZ/mens-airmax-alpha-removebg-preview.png",
+  desc: "Sahmed & sons",
+  price: "2 299",
+},
+{
+  id: 4,
+  image:"https://i.postimg.cc/Fz2vp0xN/Men-s-adidas-DROPSET-TRAINER-GREY-removebg-preview.png",
+  desc: "Police Menso Watch clean silver fang for men",
+  price: "2 199",
+},
+{
+  id: 5,
+  image:"https://i.postimg.cc/Pf85pqyT/adidas-Originals-Men-s-NMD-Black-Sneaker-removebg-preview.png",
+  desc: "Police Malawi Multifunction Leather strap",
+  price: "2 299",
+},
+
+{
+id:9,
+image:"https://i.postimg.cc/GmPRP8Tn/Men-s-adidas-D-Rose-Son-of-Chi-2-0-Black-Shoes-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"2 599",
+
+},
+{
+    id:6,
+    image:"https://i.postimg.cc/J7gHvg64/images-7-removebg-preview.png",
+    desc:"Police Raho Watch skull addition for men",
+    price:"2 999",
+
+},
+{
+  id:7,
+  image:"https://i.postimg.cc/W4HJVHNn/AU-phantom-removebg-preview.png",
+  desc:"Police Raho Watch skull addition for men",
+  price:"3 999",
+
+},
+{
+id:8,
+image:"https://i.postimg.cc/JzyLg6Fm/44cf65edbd11302198b3f9e5f0683503-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"2 399",
+
+},
+
+{
+id:10,
+image:"https://i.postimg.cc/RF0hWQF0/New-balance-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"1 899",
+
+},
+{
+id:11,
+image:"https://i.postimg.cc/7ZyL3y6d/new-balance-men-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"4 399",
+
+},
+{
+id:12,
+image:"https://i.postimg.cc/yNpgXr2R/new-balance-grey-removebg-preview.png",
+desc:"Police Raho Watch skull addition for men",
+price:"1 399",
+
+},
+];
+
+  
+  let dispItem = document.querySelector(".Products");
+  Items.forEach((data) => {
+    dispItem.innerHTML += `
+      <div class= "card  col-lg-4 col-12 col-md-4 col-sm-6 m-4 my-5">
+      <img src="${data.image}">
+      <div class="card-body">
+      <p class="text-black">${data.desc} </p>
+      <p class="prices <br>">ZAR ${data.price}</p>
+      <p class="prices <br>"> ${data.brand}</p>
+      </div>
+      </div>`;
+  });
+
+  let dispItems = document.querySelector(".Products");
+  let filteredItems = [];
+
+function displayItems(items) {
+  dispItem.innerHTML = '';
+
+  if (items.length === 0) {
+    dispItem.innerHTML = '<h1 class"mx-4" >No items found.</h1>';
+  } else {
+    items.forEach((data) => {
+      dispItem.innerHTML += `
+        <div class="card col-lg-4 col-12 col-md-4 col-sm-6 m-4 my-5">
+          <img src="${data.image}">
+          <div class="card-body">
+            <p class="text-black">${data.desc}</p>
+            <p class="prices <br>">ZAR ${data.price}</p>
+            <p class="prices <br">${data.brand}</p>
+          </div>
+        </div>`;
+    });
+  }
+
+  
+}
+
+function filterItems() {
+  const minPrice = document.getElementById('min-price').value;
+  const maxPrice = document.getElementById('max-price').value;
+
+  filteredItems = Items.filter((data) => {
+    const price = parseInt(data.price.replace(/\s/g, ''));
+    return price >= minPrice && price <= maxPrice;
+  });
+
+  displayItems(filteredItems);
+}
+
+// Display all items initially
+displayItems(Items);
+
+// Event listener for the filter button
+document.getElementById('filter-button').addEventListener('click', filterItems);
+
+// Event listener for the clear filter button
+document.getElementById('clear-filter-button').addEventListener('click', () => {
+  displayItems(Items);
+});
+
+// Caps
